eb4810c96bb6f6699a0da4c442fbb2690c241428
jhonh10 <jhonhpalacios09@gmail.com>
2022-07-14 21:30:32 -0500

    fix untracked files

--- firebase/client.js
+++
@@ -1,9 +0,0 @@
-import firebase from "firebase";
-
-const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG);
-
-!firebase.apps.length && firebase.initializeApp(firebaseConfig);
-
-const db = firebase.firestore();
-
-export {db};
