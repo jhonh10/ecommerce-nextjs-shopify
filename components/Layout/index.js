4d3b906057fd43cc070f4997828f5fbc8d22005a
jhonh10 <jhonhpalacios09@gmail.com>
2022-09-21 10:34:30 -0500

    update title and description

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -7,10 +7,10 @@ export default function Layout({ title, description, children }) {
     return (
         <>
             <Head>
-                <title>{title ? title : "Electronica y Montajes"} | {description}</title>
+                <title>{title ? title : "Bonita Store"} | {description}</title>
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-                <meta name="description" content={description || "React.js Boilerplate"} />
+                <meta name="description" content={description || "Next Js Ecommerce"} />
             </Head>
             <TopBar />
             <Header />
